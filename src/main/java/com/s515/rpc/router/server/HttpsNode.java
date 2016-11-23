af98908af62a6bb12ce8d1e77cf3b03a769b1cf9
ryan.cao <ryan.cao@airparking.com>
2016-11-23 11:55:50 +0800

    add https support

--- src/main/java/com/s515/rpc/router/server/HttpsNode.java
+++ src/main/java/com/s515/rpc/router/server/HttpsNode.java
@@ -3,19 +3,19 @@ package com.s515.rpc.router.server;
 /**
  * Created by Administrator on 8/31/2016.
  */
-public class HttpNode extends Node {
-    private final static String URL_PREFIX = "http://";
+public class HttpsNode extends Node {
+    private final static String URL_PREFIX = "https://";
     
-    public HttpNode(String appName, Host host) {
+    public HttpsNode(String appName, Host host) {
 		super(appName, host);
 	}
 
-	public HttpNode(String appName, int weight, Host host) {
+	public HttpsNode(String appName, int weight, Host host) {
 		super(appName, weight, host);
 	}
     
 
-    @Override
+	@Override
     public String getNodeUrl() {
         StringBuffer url = new StringBuffer();
         url.append(URL_PREFIX);
